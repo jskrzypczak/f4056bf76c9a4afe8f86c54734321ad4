b2ef1e955f1976e39608dde100f05da8cf1c2464
jskrzypczak <jskrzypczak97@gmail.com>
2017-06-16 19:31:50 +0200

    starting project

---
+++ Projekt winda/stdafx.cpp
@@ -0,0 +1,13 @@
+// stdafx.cpp : source file that includes just the standard includes
+// projekt.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+#define WIN32_LEAN_AND_MEAN
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
+#include <windows.h>
+#include <gdiplus.h>
+using namespace Gdiplus;
